77e7f169c05589635c2302a206dade10ace4d27f
kalitrack18 <kalitrack354@gmail.com>
2020-11-28 12:06:58 +0800

    materi genericMethod 28Nov2020

---
+++ src/com/dicoding/javafundamental/Exception/Main.java
@@ -0,0 +1,25 @@
+package com.dicoding.javafundamental.Exception;
+
+public class Main {
+
+    /**
+     * Exception adalah event (kejadian) yang mengacaukan jalannya suatu program. Worst case scenario ketika suatu
+     * program mengalami exception termination. Termination (penutupan) program adalah hal yang harus dihindari. Untuk
+     * itu kita harus menangani exception yang terjadi di program, atau yang biasa disebut sebagai handle exception.
+     *
+     * Kode yang baik adalah yang terhindar dari segala bentuk kejadian yang menyebabkan efek buruk kepada program.
+     * oleh karena itu mari kita kenali dulu berbagai macam exception yang ada di java
+     *
+     * Pada dasarnya ada 3 jenis exception berdasarkan kategorinya.
+     *
+     * 1.   Checked Exception, adalah exception yang terjadi saat compile time. Disini programmer harus menambahkan
+     *      kode untuk menghandle exception kategori ini. Jika tidak di handle maka kode yang dituliskan akan error dan tidak akan
+     *      bisa di kompilasi. Contohnya adalah exception java.io.FileNotFoundException
+     * 2.   Unchecked Exception, adalah exception yang terjadi saat execution time. Error ini terjadi dalam lingkup
+     *      dari aplikasi kita, biasanya terjadi karena salah penulisan kode atau penggunaan salah terhadap satu API. Contohnya
+     *      adalah NUllPointerException.
+     * 3.   Error, adalah exception yang diluar kendali user atau programmer. Error ini terjadi di lingkup eksternal dari aplikasi
+     *      kita. Ketika exception ini terjadi maka tidak ada yang bisa kita lakukan untuk mengatasinya, Contohnya ketika perangkat kerasnya
+     *      rusak saat kita ingin membaca data.
+     */
+}
--- src/com/dicoding/javafundamental/generics/GenericMethod.java
+++ src/com/dicoding/javafundamental/generics/GenericMethod.java
@@ -1,5 +1,6 @@
 package com.dicoding.javafundamental.generics;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class GenericMethod {
@@ -11,6 +12,37 @@ public class GenericMethod {
 
     public static void main(String[] args){
         Object[] oa = new Object[100];
+        Collection<Object> co = new ArrayList<>();
+
+        // T inferred to be Object
+        arrayToCollection(oa,co);
+
+        String[] sa = new String[100];
+        Collection<String> cs = new ArrayList<>();
+
+        // T inferred to be String
+        arrayToCollection(sa, co);
+
+        Integer[] ia = new Integer[100];
+        Float[] fa = new Float[100];
+        Number[] na = new Number[100];
+        Collection<Number> cn = new ArrayList<>();
+
+        // T inferred to be Number
+        arrayToCollection(ia, cn);
+
+        // T inferred to be Number
+        arrayToCollection(fa, cn);
+
+        // T inferred to be Number
+        arrayToCollection(na, cn);
+
+        // T inferred to be Object
+        arrayToCollection(na, co);
+
+        //compile-error
+        //arrayToCollection(na, cs);
+
     }
 
 }
