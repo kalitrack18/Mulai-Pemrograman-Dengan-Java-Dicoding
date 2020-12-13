e59b58ba4f91e6804544c8910ee31d1242a95c1c
kalitrack18 <kalitrack354@gmail.com>
2020-12-14 06:51:02 +0800

    Program bangundatar

--- src/com/dicoding/javafundamental/ProgramBangunDatar/Lingkaran.java
+++ src/com/dicoding/javafundamental/ProgramBangunDatar/Lingkaran.java
@@ -1,2 +1,21 @@
-package com.dicoding.javafundamental.ProgramBangunDatar;public class Lingkaran {
+package com.dicoding.javafundamental.ProgramBangunDatar;
+
+public class Lingkaran implements BangunDatar{
+    Lingkaran(){
+
+    }
+
+    @Override
+    public double luas(int s1, int s2) {
+        return 0;
+
+    }
+
+    @Override
+    public double keliling(int s1, int s2) {
+        return 0;
+    }
+
+    
+
 }
