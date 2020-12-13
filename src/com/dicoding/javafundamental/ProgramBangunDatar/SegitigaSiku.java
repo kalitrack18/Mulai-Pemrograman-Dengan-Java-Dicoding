a729ff6f55d7588fd4bfe9ed23bb8aa47e2841a5
kalitrack18 <kalitrack354@gmail.com>
2020-12-13 09:53:09 +0800

    ProgramBanugnDatar

---
+++ src/com/dicoding/javafundamental/ProgramBangunDatar/SegitigaSiku.java
@@ -0,0 +1,18 @@
+package com.dicoding.javafundamental.ProgramBangunDatar;
+
+public class SegitigaSiku implements BangunDatar{
+
+    SegitigaSiku(){
+
+    }
+
+    @Override
+    public double luas(int s1, int s2) {
+        return 0;
+    }
+
+    @Override
+    public double keliling(int s1, int s2) {
+        return 0;
+    }
+}
